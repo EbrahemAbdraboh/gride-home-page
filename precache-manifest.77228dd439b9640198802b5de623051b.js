4a84e12adbdd97a64e506376982e92e602f3f42e
EbrahemAbdraboh <ebrahem.abdraboh@gmail.com>
2020-08-07 18:39:26 +0200

    Updates

---
+++ precache-manifest.77228dd439b9640198802b5de623051b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ca8d80337eee582a839d88e08a039a",
+    "url": "/gride-home-page/index.html"
+  },
+  {
+    "revision": "3f7680926cf0ae5d9436",
+    "url": "/gride-home-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b57f52470ebf348ef38",
+    "url": "/gride-home-page/static/js/2.6c01fe5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gride-home-page/static/js/2.6c01fe5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7680926cf0ae5d9436",
+    "url": "/gride-home-page/static/js/main.f8bd7744.chunk.js"
+  },
+  {
+    "revision": "f0a33531fc7830995bdb",
+    "url": "/gride-home-page/static/js/runtime-main.252698ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gride-home-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
